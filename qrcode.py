658abf42ce37c4f291024ee4b3dd62c032194f7f
gh0sttxx <gh0stxx.ytt@gmail.com>
2022-12-11 06:54:20 +0000

    qrcode v1

--- qrcode.py
+++ qrcode.py
@@ -1,9 +1,60 @@
-import tkinter as tk
-import qrcode
+from tkinter import *
+from tkinter import messagebox
+import pyqrcode
 
-root = tk.Tk()
+qrcodegnr = Tk()
+qrcodegnr.title("QrCode by @gh0stxx")
+qrcodegnr.config(bg='#F25252')
 
-root.title("QrCode")
+def generate_QR():
+    if len(user_input.get())!=0 :
+        global qr,img
+        qr = pyqrcode.create(user_input.get())
+        img = BitmapImage(data = qr.xbm(scale=8))
+    else:
+        messagebox.showwarning('AVISO!', 'você deve inserir um link.')
+    try:
+        display_code()
+    except:
+        pass
+
+def display_code():
+    img_lbl.config(image = img)
+    output.config(text="scan qrcode ")
 
 
-root.mainloop()
+lbl = Label(
+    qrcodegnr,
+    text="adiciona aqui o url",
+    bg='#F25252'
+    )
+lbl.pack()
+
+user_input = StringVar()
+entry = Entry(
+    qrcodegnr,
+    textvariable = user_input
+    )
+entry.pack(padx=10)
+
+
+button = Button(
+    qrcodegnr,
+    text = "Gerar QrCode",
+    width=15,
+    command = generate_QR
+    )
+button.pack(pady=10)
+
+img_lbl = Label(
+    qrcodegnr,
+    bg='#F25252')
+img_lbl.pack()
+output = Label(
+    qrcodegnr,
+    text="",
+    bg='#F25252'
+    )
+output.pack()
+ 
+qrcodegnr.mainloop()
